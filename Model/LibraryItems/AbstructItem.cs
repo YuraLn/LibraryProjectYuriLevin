c6d8ed67867a799fcd8ac886900461657c259ba1
Yuri <eyuralevin@gmail.com>
2022-05-22 16:53:37 +0300

    Added comment

---
+++ Model/LibraryItems/AbstructItem.cs
@@ -0,0 +1,21 @@
+﻿using Model.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.LibraryItems
+{
+    public class AbstructItem
+    {
+        public string Date { get; set; }
+        public string Title { get; set; }    
+        public AbstructItem(string title)
+        {
+            //Add date on implamintation
+            Date = DateTime.UtcNow.ToString("dd-MM-yyyy");
+            Title = title;
+        }   
+    }
+}
