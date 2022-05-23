1c81e3a22ca57f315393ef507679bff759234476
Yuri <eyuralevin@gmail.com>
2022-05-23 18:52:16 +0300

    Changed reference to Model Removed as of currently viewModel props for list

---
+++ LibraryProjectYuriLevin/ViewModel/ItemCollectionViewModel.cs
@@ -0,0 +1,35 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using Model.LibraryItems;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using View.Windows;
+
+namespace LibraryProjectYuriLevin.ViewModel
+{
+    public class ItemCollectionViewModel : ViewModelBase
+    {
+        public RelayCommand AddWindowCommand { get; set; }
+        public RelayCommand SearchWindowCommand { get; set; }
+        public RelayCommand EditWindowCommand { get; set; }
+        public ItemCollection Items { get; set; }
+        
+
+        public ItemCollectionViewModel()
+        {
+            AddWindowCommand = new RelayCommand(AddWindowBook);
+
+            //SearchWindowCommand = new RelayCommand(SearchWindow);
+            //EditWindowCommand = new RelayCommand(EditWindow);
+            //Items = Instance;
+            //OCShowList = Items.Items;
+        }
+
+        private void AddWindowBook()
+        {
+            var open = new AddBookWindow();
+            open.Show();
+        }
+    }
+}
---
+++ Model/ItemCollection.cs
@@ -0,0 +1,14 @@
+﻿using Model.LibraryItems;
+using System.Collections.ObjectModel;
+
+namespace Model
+{
+    internal class ItemCollection
+    {
+        public ItemCollection Items { get; set; }
+        public ObservableCollection<AbstructItem> ListToShow{ get; set; }
+
+
+        
+    }
+}
